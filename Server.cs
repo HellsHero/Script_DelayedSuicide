6efbed54ee534d52ad490e7b992db9556852cc49
HellsHero <tristanturcotte@gmail.com>
2014-02-12 20:52:03 -0500

    Fixed various issues
    This is the finalized version

--- Server.cs
+++ Server.cs
@@ -1,11 +1,12 @@
-$Server::delaySuicide = 1; //Enable/disable script
-$Server::delaySuicide::time = 10; //In seconds
-package script_delaySuicide
+$Server::delayedSuicide = 1; //Enable/disable script
+$Server::delayedSuicide::time = 10; //In seconds
+package script_delayedSuicide
 {
     function serverCmdSuicide(%client)
     {
-        if(isObject(%pl = %client.player))
-            if(isObject(%client.minigame) && $Server::delaySuicide && !%pl.canSuicide)
+        %pl = %client.player;
+        if(isObject(%pl))
+            if(isObject(%client.minigame) && $Server::delayedSuicide && !%pl.canSuicide)
                 %pl.suicideSequence(0);
             else
                 parent::serverCmdSuicide(%client);
@@ -13,68 +14,69 @@ package script_delaySuicide
     
     function Armor::damage(%this, %obj, %sourceObject, %position, %damage, %damageType)
 	{
-	    if(isObject(%obj) && isObject(%obj.client.minigame) && $Server::delaySuicide)
-	        %pl.suicideSequence(1);
+	    if(isObject(%obj) && isObject(%obj.client.minigame) && $Server::delayedSuicide)
+	        if(isEventPending(%obj.suicideDelay))
+	            %obj.suicideSequence(1);
 		Parent::damage(%this, %obj, %sourceObject, %position, %damage, %damageType);
 	}
 };
-activatePackage(script_delaySuicide);
+activatePackage(script_delayedSuicide);
 
 function Player::suicideSequence(%this,%cancel)
 {
-    if(isEventPending(%pl.suicideDelay))
-        cancel(%pl.suicideDelay);
+    if(isEventPending(%this.suicideDelay))
+        cancel(%this.suicideDelay);
     
-    %client = %pl.client;
+    %client = %this.client;
     if(%cancel)
     {
-        messageClient(%client,'',"\c6Suicide canceled! ( " @ %pl.suicideSequence @ "/" @ $Server::delaySuicide::time @ " )");
-        %pl.suicidePos = "";
-        %pl.suicideSequence = "";
+        messageClient(%client,'',"\c6Suicide canceled!");
+        %this.suicidePos = "";
+        %this.suicideSequence = "";
         return;
     }
     
-    if(%pl.suicideSequence $= "" || %pl.suicideSequence == 0)
+    if(%this.suicideSequence $= "" || %this.suicideSequence == 0)
     {
-        echo("Suicide Sequence, initial go through" SPC %pl.suicideSequence SPC ":" SPC %pl.suicidePos);
-        %pl.suicideSequence = 0;
-        %pl.suicidePos = %pl.getPosition();
+        echo("Suicide Sequence, initial go through" SPC %this.suicideSequence SPC ":" SPC %this.suicidePos);
+        %this.suicideSequence = 0;
+        %this.suicidePos = %this.getPosition();
     }
     
-    if(vectorDist(%pl.getPosition(),%pl.suicidePos) > 0.5)
+    if(vectorDist(%this.getPosition(),%this.suicidePos) != 0)
     {
-        messageClient(%client,'',"\c6Suicide canceled! ( " @ %pl.suicideSequence @ "/" @ $Server::delaySuicide::time @ " )");
-        %pl.suicidePos = "";
-        %pl.suicideSequence = "";
+        messageClient(%client,'',"\c6Suicide canceled!");
+        %this.suicidePos = "";
+        %this.suicideSequence = "";
         return;
     }
     
-    if(%pl.suicideSequence == $Server::delaySuicide::time)
+    if(%this.suicideSequence == $Server::delayedSuicide::time)
     {
-        %pl.suicideSequence = "";
-        %pl.suicidePos = "";
-        %pl.canSuicide = 1;
+        %this.suicideSequence = "";
+        %this.suicidePos = "";
+        %this.canSuicide = 1;
         call(serverCmdSuicide(%client));
         return;
     }
-    %timeLeft = $Server::delaySuicide::time-%pl.suicideSequence;
+    %timeLeft = $Server::delayedSuicide::time-%this.suicideSequence;
     messageClient(%client,'',"\c6Suicide in " @ %timeleft SPC ((%timeLeft == 1) ? "second" : "seconds"));
-    %pl.suicideSequence++;
+    %this.suicideSequence++;
     
-    %pl.suicideDelay = %pl.schedule(1000,suicideSequence,%cancel);
+    %this.suicideDelay = %this.schedule(1000,suicideSequence,%cancel);
 }
 
-function serverCmdDelaySuicide(%client,%a)
+function serverCmdDelayedSuicide(%client,%a)
 {
     if(!%client.isAdmin)
         return;
     if(%a $= "" && %b $= "")
     {
-        messageClient(%client,'',"\c6Type \c3/delaySuicide A");
+        messageClient(%client,'',"\c6Type \c3/delayedSuicide A");
         messageClient(%client,'',"\c3A \c6can be replaced with \c3toggle\c6 or a \c3number\c6 which represents seconds");
     }
     else if(%a $= "toggle")
-        messageClient(%client,'',"\c6DelaySuicide script is now\c3 " @ (($Server::delaySuicide = !$Server::delaySuicide) ? "enabled" : "disabled"));
+        messageClient(%client,'',"\c6DelayedSuicide script is now\c3 " @ (($Server::delayedSuicide = !$Server::delayedSuicide) ? "enabled" : "disabled"));
     else if(%a > 0)
-        messageClient(%client,'',"\c6DelaySuicide time is now\c3 " @ ($Server::delaySuicide::time = %a));
+        messageClient(%client,'',"\c6DelayedSuicide time is now\c3 " @ ($Server::delayedSuicide::time = %a));
 }
